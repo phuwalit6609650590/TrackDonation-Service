ccde6fcbc1a34edea20ea899a5e51717e4c2d1e8
Phuwalit <phuwalit.jar@dome.tu.ac.th>
2026-03-08 20:54:28 +0700

    Initial project setup

---
+++ TrackDonation/TrackDonation/src/main/java/com/project/trackdonation/TrackDonationApplication.java
@@ -0,0 +1,13 @@
+package com.project.trackdonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackDonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackDonationApplication.class, args);
+	}
+
+}
---
+++ TrackDonation/TrackDonation/src/test/java/com/project/trackdonation/TrackDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trackdonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
